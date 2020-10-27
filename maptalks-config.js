2fa1db903230b5a5e3620a36764087834e875c63
Krishna Guru <36165748+KG-11@users.noreply.github.com>
2020-10-27 22:35:28 +0530

    Add files via upload

---
+++ maptalks-config.js
@@ -0,0 +1,8 @@
+const maptalksConfig = {};
+
+maptalksConfig.urlTemplate = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+maptalksConfig.subdomains = ['a', 'b', 'c', 'd'];
+maptalksConfig.attribution = '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
+maptalksConfig.centerCoords = [73.8623472, 15.5040444];
+maptalksConfig.centerlat = 15.5040444;
+maptalksConfig.centerlon = 73.8623472;
